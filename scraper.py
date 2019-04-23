55a9b32546e31c15db8cc0904731633be6f035fd
Gianna Aprile <giannaaprile99@gmail.com>
2019-04-22 21:48:58 -0400

    setting up soup

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+
+url = 'http://sis.rutgers.edu/soc/#subjects?semester=92019&campus=NB&level=U'
+
+page = urllib2.urlopen(url)
+
+soup = BeautifulSoup(page, ‘html.parser’)
+
+print(soup)
+
