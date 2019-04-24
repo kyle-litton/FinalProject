914aa39137ae3b6b7e1241b84b0dc0ada04b23a1
jkl1999 <99kylel@gmail.com>
2019-04-24 12:39:09 -0400

    added chromedriver

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver_path = "/Users/kylelitton/Downloads/chromedriver"
+driver_path = "/Users/kylelitton/FinalProject/chromedriver"
 browser = webdriver.Chrome(driver_path)
 
 browser.get("http://sis.rutgers.edu/soc/#subjects?semester=92019&campus=NB&level=U")
